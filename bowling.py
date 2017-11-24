48f34d913cc2bc230f4586be2d8f23ddf5b4c478
Csonti <csonti515@gmail.com>
2017-11-24 19:29:37 +0100

    get values changed for bonuses and functions that calls get values changed becouse of it

--- bowling.py
+++ bowling.py
@@ -8,15 +8,10 @@ def score(game):
     first_ball_of_frame = True
     for i in range(len(game)):
 
-        if is_spare(game[i]):
-            result += ALL_PINS - last
-        else:
-            result += get_value(game[i])
+        result += get_value(game, i)
 
-        if frame < NORMAL_FRAMES and get_value(game[i]) == ALL_PINS:
+        if frame < NORMAL_FRAMES and (is_spare(game[i]) or is_strike(game[i])):
             result += get_bonus(game, i)
-            
-        last = get_value(game[i])
 
         if not first_ball_of_frame:
             frame += 1
@@ -30,16 +25,17 @@ def score(game):
     return result
 
 
-def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
-        return int(char)
-    elif is_strike(char):
+def get_value(game, ball_index):
+    ball = game[ball_index]
+    if ball == '1' or ball == '2' or ball == '3' or \
+       ball == '4' or ball == '5' or ball == '6' or \
+       ball == '7' or ball == '8' or ball == '9':
+        return int(ball)
+    elif is_strike(ball):
         return ALL_PINS
-    elif is_spare(char):
-        return ALL_PINS
-    elif char == '-':
+    elif is_spare(ball):
+        return ALL_PINS - get_value(game, ball_index - 1)
+    elif ball == '-':
         return 0
     else:
         raise ValueError()
@@ -56,12 +52,9 @@ def is_spare(ball):
 def get_bonus(game, ball_index):
     bonus = 0
     if is_spare(game[ball_index]):
-        bonus += get_value(game[ball_index+1])
+        bonus += get_value(game, ball_index+1)
     elif is_strike(game[ball_index]):
-        bonus += get_value(game[ball_index+1])
-        if is_spare(game[ball_index+2]):
-            bonus += ALL_PINS - get_value(game[ball_index+1])
-        else:
-            bonus += get_value(game[ball_index+2])
+        bonus += get_value(game, ball_index+1)
+        bonus += get_value(game, ball_index+2)
     return bonus
 
