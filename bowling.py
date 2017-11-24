e2d388d36939a1235827d06a82ab12f4899db7a4
Csonti <csonti515@gmail.com>
2017-11-24 18:13:57 +0100

    constant for all pins

--- bowling.py
+++ bowling.py
@@ -1,21 +1,24 @@
+ALL_PINS = 10
+
+
 def score(game):
     result = 0
     frame = 1
     in_first_half = True
     for i in range(len(game)):
         if game[i] == '/':
-            result += 10 - last
+            result += ALL_PINS - last
         else:
             result += get_value(game[i])
         # if not in_first_half:
             # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
+        if frame < ALL_PINS and get_value(game[i]) == ALL_PINS:
             if game[i] == '/':
                 result += get_value(game[i+1])
             elif game[i] == 'X' or game[i] == 'x':
                 result += get_value(game[i+1])
                 if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+                    result += ALL_PINS - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
         last = get_value(game[i])
@@ -30,16 +33,18 @@ def score(game):
             frame += 1
     return result
 
+
 def get_value(char):
     if char == '1' or char == '2' or char == '3' or \
        char == '4' or char == '5' or char == '6' or \
        char == '7' or char == '8' or char == '9':
         return int(char)
     elif char == 'X' or char == 'x':
-        return 10
+        return ALL_PINS
     elif char == '/':
-        return 10
+        return ALL_PINS
     elif char == '-':
         return 0
     else:
         raise ValueError()
+
